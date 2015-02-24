1438d84d60b29fa11c104d22611369bccc645543
Charles Ruhland <cruhland@alumni.cmu.edu>
2015-02-24 09:32:15 -0500

    Partial documentation for `fd` module

--- src/fd.rs
+++ src/fd.rs
@@ -1,16 +1,81 @@
 
+//! Provides operations on file descriptors.
+
 use std::ffi;
 use std::os;
 use libc::{open, read, write, close, c_int, size_t, mode_t, c_void};
 use err::Errno;
 
+/// The result of a system call.
+pub type SysResult<T> = Result<T, Errno>;
+
+/// Newtype for file descriptors.
+///
+/// Does not implement `Copy` so that `FileDescriptor::close()` can
+/// take ownership, preventing file descriptors from being used
+/// afterwards.
+pub struct FileDescriptor {
+    raw: c_int,
+}
+
+/// Factors out the common operation of creating a `SysResult` based
+/// on a syscall return value and `errno`.
 macro_rules! errno_check {
     ($status:expr, $success:expr) => (
         if $status == -1 { Err(Errno::new(os::errno())) } else { Ok($success) }
     )
 }
 
-pub type SysResult<T> = Result<T, Errno>;
+impl FileDescriptor {
+
+    /// The `open` system call.
+    ///
+    /// ## Arguments
+    ///
+    /// - `path`: the pathname of the file to open. Ownership is
+    /// transferred to this function so that it can be converted into
+    /// a C-style string.
+    /// - `flags`: specifies the access mode, file creation flags, and
+    /// file status flags using a dedicated type.
+    /// - `mode`: specifies the permissions to give the file if it is
+    /// being created, using a dedicated type. If the file is not
+    /// being created, an empty set of flags should be supplied.
+    ///
+    /// Returns a new file descriptor on success, or an appropriate
+    /// `Errno` on failure.
+    ///
+    /// Consult the manpage (command `man 2 open`) for further
+    /// details.
+    pub fn open(
+        path: String, flags: OpenFlags, mode: FilePerms
+    ) -> SysResult<FileDescriptor> {
+        let cstring_path = ffi::CString::from_vec(path.into_bytes()).as_ptr();
+        let fd = unsafe { open(cstring_path, flags.bits(), mode.bits()) };
+        errno_check!(fd, FileDescriptor { raw: fd, })
+    }
+
+    pub fn read(&self, buf: &mut [u8]) -> SysResult<usize> {
+        let buf_ptr = buf.as_mut_ptr() as *mut c_void;
+        let buf_len = buf.len() as size_t;
+        let bytes_read = unsafe { read(self.raw, buf_ptr, buf_len) };
+        errno_check!(bytes_read, bytes_read as usize)
+    }
+
+    pub fn write(&self, buf: &[u8]) -> SysResult<usize> {
+        let buf_ptr = buf.as_ptr() as *const c_void;
+        let buf_len = buf.len() as size_t;
+        let bytes_written = unsafe { write(self.raw, buf_ptr, buf_len) };
+        errno_check!(bytes_written, bytes_written as usize)
+    }
+
+    // We cannot safely provide a Drop impl to handle this automatically;
+    // it does not provide a mechanism for handling errors
+    pub fn close(self) -> SysResult<()> {
+        let status = unsafe { close(self.raw) };
+        errno_check!(status, ())
+    }
+
+}
 
 // open() flags on my x86-64 Linux system
 // Not intended to be portable!
@@ -58,44 +123,4 @@ bitflags! {
         const S_IRWXG = S_IRGRP.bits | S_IWGRP.bits | S_IXGRP.bits,
         const S_IRWXO = S_IROTH.bits | S_IWOTH.bits | S_IXOTH.bits,
     }
-}
-
-pub struct FileDescriptor(c_int);
-
-impl FileDescriptor {
-
-    pub fn open(
-        path: String, flags: OpenFlags, mode: FilePerms
-    ) -> SysResult<FileDescriptor> {
-        let cstring_path = ffi::CString::from_vec(path.into_bytes()).as_ptr();
-        let fd = unsafe { open(cstring_path, flags.bits(), mode.bits()) };
-        errno_check!(fd, FileDescriptor(fd))
-    }
-
-    pub fn read(&self, buf: &mut [u8]) -> SysResult<usize> {
-        let buf_ptr = buf.as_mut_ptr() as *mut c_void;
-        let buf_len = buf.len() as size_t;
-        let bytes_read = unsafe { read(self.raw(), buf_ptr, buf_len) };
-        errno_check!(bytes_read, bytes_read as usize)
-    }
-
-    pub fn write(&self, buf: &[u8]) -> SysResult<usize> {
-        let buf_ptr = buf.as_ptr() as *const c_void;
-        let buf_len = buf.len() as size_t;
-        let bytes_written = unsafe { write(self.raw(), buf_ptr, buf_len) };
-        errno_check!(bytes_written, bytes_written as usize)
-    }
-
-    // We cannot safely provide a Drop impl to handle this automatically;
-    // it does not provide a mechanism for handling errors
-    pub fn close(self) -> SysResult<()> {
-        let status = unsafe { close(self.raw()) };
-        errno_check!(status, ())
-    }
-
-    fn raw(&self) -> c_int {
-        let &FileDescriptor(fd) = self;
-        fd
-    }
-
 }
