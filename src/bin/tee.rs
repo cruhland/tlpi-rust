f3afc63811dfc902b0801f85b4df09a62284b6fe
Charles Ruhland <cruhland@alumni.cmu.edu>
2015-03-11 06:32:47 -0700

    Exercise 4-1: parsing command-line options

--- src/bin/tee.rs
+++ src/bin/tee.rs
@@ -1,10 +1,14 @@
 
-#![feature(libc, exit_status)]
+#![feature(libc, exit_status, collections)]
 
 #[macro_use]
 extern crate tlpi_rust;
 
+extern crate getopts;
+use getopts::Options;
+
 use tlpi_rust::err::*;
+use std::env;
 
 fn main() {
     set_exit_status!(main_with_result());
@@ -13,6 +17,16 @@ fn main() {
 fn main_with_result() -> TlpiResult<()> {
     // TODO
     // Interpret command-line arguments
+    let argv: Vec<_> = env::args().collect();
+    let opts = build_options();
+    let matches = match opts.parse(argv.tail()) {
+        Ok(m) => m,
+        Err(f) => {
+            let usage = opts.usage(&f.to_err_msg()[..]);
+            return cmd_line_err!("{}", usage)
+        },
+    };
+
     // Open destination file (truncate or append)
     // Until EOF on stdin:
     //   Read a chunk of bytes from stdin
@@ -20,6 +34,13 @@ fn main_with_result() -> TlpiResult<()> {
     //   Write that chunk to destination file
     // Close destination file
 
-    println!("*** NOT IMPLEMENTED ***");
+    println!("Matches: {:?}", matches.free);
     Err(())
+}
+
+fn build_options() -> Options {
+    let mut opts = Options::new();
+    opts.optflag("h", "help", "display this usage message");
+    opts.optflag("a", "append", "append output instead of truncating");
+    opts
 }
